d2bc78cd3bb69f180dfbaeb26805e50b97a26552
juanurban <63820354+juanurban@users.noreply.github.com>
2020-04-22 14:27:08 -0500

    Adptación codigo - juan

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -4,9 +4,9 @@ class Conexion{
 
 	static public function conectar(){
 
-		$link = new PDO("mysql:host=localhost;dbname=kmargoco_pos",
-			            "kmargoco_wp94",
-			            "Morris*0211");
+		$link = new PDO("mysql:host=localhost;dbname=pos",
+			            "root",
+			            "");
 
 		$link->exec("set names utf8");
 
