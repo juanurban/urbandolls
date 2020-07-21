ddbc3f2a4613e141ddad489c099b2df9b8c381bc
Sofía Pérez Mantilla <32691511+sofiaperezm@users.noreply.github.com>
2020-07-20 20:26:09 -0500

    Anadir id_usuario al log
    Modifcacion del archivo modelo logs

--- modelos/log.modelo.php
+++ modelos/log.modelo.php
@@ -9,19 +9,17 @@ class ModeloLogs {
 	=============================================*/
 	static public function mdlRegistrarLogs($tabla, $datos){
 
-		$stmt = Conexion::conectar()->prepare("INSERT INTO $tabla(etiqueta, descripcion, registro) VALUES (:etiqueta, :descripcion, NOW())");
+		$id = isset($_SESSION["id"]) ? $_SESSION["id"] : null;
+		$stmt = Conexion::conectar()->prepare("INSERT INTO $tabla(etiqueta, descripcion, registro, usuario_id) VALUES (:etiqueta, :descripcion, NOW(), :usuario_id)");
 
 		$stmt->bindParam(":etiqueta", $datos["etiqueta"], PDO::PARAM_STR);
 		$stmt->bindParam(":descripcion", $datos["descripcion"], PDO::PARAM_STR);
+		$stmt->bindParam(":usuario_id", $id, PDO::PARAM_STR);
 	
 		if($stmt->execute()){
-
 			return "ok";
-
 		}else{
-
 			return "error";
-		
 		}
 
 		$stmt->close();
